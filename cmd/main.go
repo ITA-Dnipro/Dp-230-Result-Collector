088cf58a13be59b9adedd2b225ad6c12cda426d1
Ig Kos <ig.fire@gmail.com>
2022-07-27 18:45:29 +0300

    refactor: change packages and app init

--- cmd/main.go
+++ cmd/main.go
@@ -3,12 +3,11 @@ package main
 import (
 	"context"
 	"log"
-	"net"
 
 	"github.com/go-playground/validator/v10"
 	"github.com/kelseyhightower/envconfig"
-	"google.golang.org/grpc"
 
+	"github.com/ITA-Dnipro/Dp-230-Result-Collector/internal/app"
 	"github.com/ITA-Dnipro/Dp-230-Result-Collector/internal/config"
 	"github.com/ITA-Dnipro/Dp-230-Result-Collector/internal/mongodb"
 	"github.com/ITA-Dnipro/Dp-230-Result-Collector/internal/service"
@@ -17,37 +16,22 @@ import (
 
 func main() {
 	var cfg config.Config
-	if err := envconfig.Process("xss", &cfg); err != nil {
+	if err := envconfig.Process("", &cfg); err != nil {
 		log.Fatal(err.Error())
 	}
+	log.Fatal(run(cfg))
+}
 
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	mongoDBConn, err := mongodb.NewMongoDB(ctx, cfg)
+func run(cfg config.Config) error {
+	app, err := app.NewApp(cfg)
 	if err != nil {
-		log.Fatal("cannot connect mongodb", err)
-	}
-	defer func() {
-		if err := mongoDBConn.Disconnect(ctx); err != nil {
-			log.Fatal("mongoDBConn.Disconnect", err)
-		}
-	}()
-
-	lis, err := net.Listen("tcp", cfg.Server.Port)
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		return err
 	}
 
 	validate := validator.New()
-	reportRepo := mongodb.NewReportMongoRepo(mongoDBConn)
+	reportRepo := mongodb.NewReportMongoRepo(app.MongoClient.Client)
 	service := service.NewReportService(reportRepo, validate)
+	pb.RegisterReportServiceServer(app.Server.Server, service)
 
-	srv := grpc.NewServer()
-	pb.RegisterReportServiceServer(srv, service)
-
-	log.Printf("starting server %s", cfg.Server.Port)
-	if err := srv.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %s", err)
-	}
-
+	return app.Run(context.Background())
 }
--- internal/mongodb/mongodb.go
+++ internal/mongodb/mongodb.go
@@ -2,24 +2,17 @@ package mongodb
 
 import (
 	"context"
-	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 
-	"github.com/ITA-Dnipro/Dp-230-Result-Collector/internal/config"
 	"github.com/ITA-Dnipro/Dp-230-Result-Collector/model"
 	"github.com/pkg/errors"
 )
 
 const (
-	connectTimeout  = 30 * time.Second
-	maxConnIdleTime = 3 * time.Minute
-	minPoolSize     = 20
-	maxPoolSize     = 300
-
 	reportsDB         = "reports"
 	reportsCollection = "reports"
 )
@@ -27,32 +20,6 @@ const (
 var (
 	ErrObjectIDTypeConversion = errors.New("object id type conversion")
 )
-
-func NewMongoDB(ctx context.Context, cfg config.Config) (*mongo.Client, error) {
-	client, err := mongo.NewClient(
-		options.Client().ApplyURI(cfg.MongoDB.URI).
-			SetAuth(options.Credential{
-				Username: cfg.MongoDB.User,
-				Password: cfg.MongoDB.Password,
-			}).
-			SetConnectTimeout(connectTimeout).
-			SetMaxConnIdleTime(maxConnIdleTime).
-			SetMinPoolSize(minPoolSize).
-			SetMaxPoolSize(maxPoolSize))
-	if err != nil {
-		return nil, err
-	}
-
-	if err := client.Connect(ctx); err != nil {
-		return nil, err
-	}
-
-	if err := client.Ping(ctx, nil); err != nil {
-		return nil, err
-	}
-
-	return client, nil
-}
 
 type reportMongoRepo struct {
 	mongoDB *mongo.Client
